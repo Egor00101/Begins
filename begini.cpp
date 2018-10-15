700beea000fdf027a930e75990d6f2b623675e82
Egor00101 <43553542+Egor00101@users.noreply.github.com>
2018-10-16 01:16:43 +0300

    Update begini.cpp

--- begini.cpp
+++ begini.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-void beg39(double a1,double a2,double b1,double b2,double c1,double c2)
+void f39(double a1,double a2,double b1,double b2,double c1,double c2)
 {
 double D=a1*b2-a2*b1;
 double x=(c1*b2-c2*b1)/D;
@@ -19,14 +19,14 @@ int main ()
   cin >> a1 >> b1 >> c1;
   cout << "vvedite kof. 2 uravnenia:=  "<<"\n";
   cin >> a2 >> b2 >> c2;
- f(a1,a2,b1,b2,c1,c2);
+ func(a1,a2,b1,b2,c1,c2);
   return 0;
 }
 40)
   #include <iostream>
 using namespace std;
 #include <cmath>
-void func(int A, int B, int C){
+void f40(int A, int B, int C){
 if(A != 0)
 {
  int Dis = pow(B,2) - 4 * A * C;
